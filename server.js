14c8e8b26d6e9f0295657f2456b7cd183d085b8c
Ibrahim Elfors <ibrahim.forse@gmail.com>
2018-07-29 01:32:04 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentDate',()=>{
+    return new Date().getFullYear();
+});
+app.set('viewengine','hbs');
+
+app.use((req, res, next)=>{
+    var now = new Date().toISOString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log+'\n',(error)=>{
+        console.log("server couldn't log error.");
+    });
+
+    next();
+});
+
+/* app.use((req,res,next)=>{
+    res.render('maintainence.hbs');
+}); */
+
+app.use(express.static(__dirname+'/public'))
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle: 'About Page'
+    });
+});
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to our webste'
+    });
+});
+
+app.get('/bad',((req,res)=>{
+    res.send({
+        code:400,
+        message:'an error occurred'
+    });
+}));
+
+app.listen(3000,()=>{
+    console.log("Server is running (:3000) ...");
+});
