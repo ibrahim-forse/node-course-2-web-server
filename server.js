e8860750c46bd2950cdbc662d36c4a5a6313b87f
Ibrahim Elfors <ibrahim.forse@gmail.com>
2018-07-29 02:23:05 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname+'/views/partials');
 hbs.registerHelper('getCurrentDate',()=>{
@@ -45,6 +45,7 @@ app.get('/bad',((req,res)=>{
     });
 }));
 
-app.listen(3000,()=>{
-    console.log("Server is running (:3000) ...");
+
+app.listen(port,()=>{
+    console.log(`Server is running (:${port}) ...`);
 });
